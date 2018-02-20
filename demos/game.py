870480a172c9b31c2d555f44f147e3ee72b4afc8
Rybec <rybecarethdar@gmail.com>
2018-02-20 23:46:30 +0000

    This has a cheap hack that needs fixed

--- demos/game.py
+++ demos/game.py
@@ -5,6 +5,9 @@ import units
 
 graphics.init()
 
+import event
+
+
 # back = graphics.load("stars.png")
 
 graphics.background = graphics.load("stars.png")
@@ -17,20 +20,25 @@ george2.facing = "left"
 graphics.register(george)
 graphics.register(george2)
 
+def quit(e):
+	global run
+	if e.type == pygame.QUIT:
+		run = False
+	elif e.type == pygame.KEYUP:
+		if ((e.key == pygame.K_F4) and
+		   (e.mod and pygame.KMOD_ALT)):
+			run = False
+			
+event.register(george.handler)
+event.register(quit)
 
 clock = pygame.time.Clock()
 run = True
 frame = 0
 while (run):
 	clock.tick(30)
-	for e in pygame.event.get():
-		if e.type == pygame.QUIT:
-			run = False
-		elif e.type == pygame.KEYUP:
-			if ((e.key == pygame.K_F4) and
-			   (e.mod and pygame.KMOD_ALT)):
-				run = False
 
+	event.update()
 	george.update()
 	george2.update()
 	graphics.update()
